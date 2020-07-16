0d2c5f017db01883fa2a3f4d155a86d3cc10cad5
Evgen <shishishi702@gmail.com>
2020-07-16 10:37:43 +0300

    cycles and conditions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('How much films do you see?', '');
 
 const personalMovieDB = {
@@ -8,13 +10,33 @@ const personalMovieDB = {
    privat: false
 };
 
-const x = prompt('What the last film did you see?'),
-   y = prompt('What rating it worthy?'),
-   a = prompt('What the last film did you see?'),
-   b = prompt('What rating it worthy?');
+if (personalMovieDB.count < 10) {
+   console.log('You see very few films!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('You are classic viewer!');
+} else if (personalMovieDB.count >= 30) {
+   console.log('You are movie fun!');
+} else {
+   console.log('Error');
+}
 
-personalMovieDB.movies[x] = y;
-personalMovieDB.movies[a] = b;
+
+
+
+for(let i = 0; 2 > i; i++) {
+   let x = prompt('What the last film did you see?'),
+       y = prompt('What rating it worthy?');
+
+         if(x === null || y === null) {
+            i--;
+         } else if (x.length > 50 || y.length > 50) {
+            i--;   
+         } else if (x === '' || y === '') {
+            i--;
+         }   else {
+            personalMovieDB.movies[x] = y;
+         }
+   
+}
 
 console.log(personalMovieDB);
-
